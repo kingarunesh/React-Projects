7df5167ec355a35672648dc57e16da34d1cc6c36
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-10-20 01:51:25 +0530

    15.2 : Not Working

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,57 +1,55 @@
+import { useState } from "react";
+
 import "./accordion.scss";
 
+const DATA = [
+    {
+        id: 0,
+        title: "What is HTML?",
+        description:
+            "HTML Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet, dolores illo sequiperspiciatis, fugiat beatae iste, quas culpa tenetur asperiores? Eius et incidunt eos illo animi vitaequod ipsam.",
+    },
+
+    {
+        id: 1,
+        title: "What is CSS?",
+        description:
+            "CSS Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet, dolores illo sequiperspiciatis, fugiat beatae iste, quas culpa tenetur asperiores? Eius et incidunt eos illo animi vitaequod ipsam.",
+    },
+
+    {
+        id: 2,
+        title: "What is JavaScript?",
+        description:
+            "JavaScript Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet, dolores illo sequiperspiciatis, fugiat beatae iste, quas culpa tenetur asperiores? Eius et incidunt eos illo animi vitaequod ipsam.",
+    },
+];
+
 export default function Accordion() {
+    const [open, setOpen] = useState(null);
+
+    const openHandler = function (id) {
+        setOpen((c) => (c === id ? null : id));
+    };
+
     return (
         <>
             <div className="accordion">
                 <div className="accordion__container">
-                    <section className="accordion__container--box">
-                        <div className="accordion__container--box--number">1</div>
+                    {DATA.map((item) => (
+                        <section className="accordion__container--box" key={item.id}>
+                            <div className="accordion__container--box--number">
+                                {item.id + 1 < 10 ? `0${item.id + 1}` : item.id}
+                            </div>
 
-                        <p className="accordion__container--box--title">What is HTML?</p>
+                            <p className="accordion__container--box--title">{item.title}</p>
 
-                        <div className="accordion__container--box--icon">
-                            <span className="material-symbols-outlined">add</span>
-                        </div>
-
-                        <p className="accordion__container--box--body">
-                            HTML Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet, dolores illo sequi
-                            perspiciatis, fugiat beatae iste, quas culpa tenetur asperiores? Eius et incidunt eos illo animi vitae
-                            quod ipsam.
-                        </p>
-                    </section>
-
-                    <section className="accordion__container--box">
-                        <div className="accordion__container--box--number">2</div>
-
-                        <p className="accordion__container--box--title">What is CSS?</p>
-
-                        <div className="accordion__container--box--icon">
-                            <span className="material-symbols-outlined">add</span>
-                        </div>
-
-                        <p className="accordion__container--box--body hidden">
-                            CSS Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet, dolores illo sequi
-                            perspiciatis, fugiat beatae iste, quas culpa tenetur asperiores? Eius et incidunt eos illo animi vitae
-                            quod ipsam.
-                        </p>
-                    </section>
-
-                    <section className="accordion__container--box ">
-                        <div className="accordion__container--box--number">3</div>
-
-                        <p className="accordion__container--box--title">What is JavaScript?</p>
-
-                        <div className="accordion__container--box--icon">
-                            <span className="material-symbols-outlined">add</span>
-                        </div>
-
-                        <p className="accordion__container--box--body hidden">
-                            JavaScript Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet, dolores illo sequi
-                            perspiciatis, fugiat beatae iste, quas culpa tenetur asperiores? Eius et incidunt eos illo animi vitae
-                            quod ipsam.
-                        </p>
-                    </section>
+                            <div className="accordion__container--box--icon" onClick={() => openHandler(item.id)}>
+                                <span className="material-symbols-outlined">add</span>
+                            </div>
+                        </section>
+                    ))}
+                    {open && <p className="accordion__container--box--body">{DATA[open].description}</p>}
                 </div>
             </div>
         </>
