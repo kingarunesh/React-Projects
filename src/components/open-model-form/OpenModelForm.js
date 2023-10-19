c71b2fc3a259778c83034c9e22b8dfd9373e0b84
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-10-19 19:27:36 +0530

    13.2 : Open Model Form -- Styel : Model Added

--- src/components/open-model-form/OpenModelForm.js
+++ src/components/open-model-form/OpenModelForm.js
@@ -4,6 +4,27 @@ export default function OpenModelForm() {
     return (
         <>
             <div className="open-model-form">
+                <div className="open-model-form__model">
+                    <span className="material-symbols-outlined">close</span>
+                    <form className="open-model-form__model--form">
+                        <div className="open-model-form__model--form--input-group">
+                            <label htmlFor="email">Username or Email</label>
+                            <input type="text" placeholder="Username or Email" id="email" />
+                        </div>
+
+                        <div className="open-model-form__model--form--input-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" placeholder="Password" id="password" autoComplete="off" />
+                        </div>
+
+                        <div className="open-model-form__model--form--input-group">
+                            <div>
+                                <button>Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
                 <header className="open-model-form__header">Header</header>
 
                 <nav className="open-model-form__nav">
