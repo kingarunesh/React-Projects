122575bf6a3a2224843f7d060a693b5c1541fa09
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-10-19 19:34:52 +0530

    13.3 : Login Model Open without overlay

--- src/components/open-model-form/OpenModelForm.js
+++ src/components/open-model-form/OpenModelForm.js
@@ -1,29 +1,41 @@
+import { useState } from "react";
+
 import "./open-model-form.scss";
 
 export default function OpenModelForm() {
+    const [loginOpen, setLoginOpen] = useState(false);
+
+    const loginOpenHandler = function () {
+        setLoginOpen((c) => !c);
+    };
+
     return (
         <>
             <div className="open-model-form">
-                <div className="open-model-form__model">
-                    <span className="material-symbols-outlined">close</span>
-                    <form className="open-model-form__model--form">
-                        <div className="open-model-form__model--form--input-group">
-                            <label htmlFor="email">Username or Email</label>
-                            <input type="text" placeholder="Username or Email" id="email" />
-                        </div>
-
-                        <div className="open-model-form__model--form--input-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" placeholder="Password" id="password" autoComplete="off" />
-                        </div>
-
-                        <div className="open-model-form__model--form--input-group">
-                            <div>
-                                <button>Login</button>
+                {loginOpen && (
+                    <div className="open-model-form__model">
+                        <span className="material-symbols-outlined" onClick={loginOpenHandler}>
+                            close
+                        </span>
+                        <form className="open-model-form__model--form">
+                            <div className="open-model-form__model--form--input-group">
+                                <label htmlFor="email">Username or Email</label>
+                                <input type="text" placeholder="Username or Email" id="email" />
                             </div>
-                        </div>
-                    </form>
-                </div>
+
+                            <div className="open-model-form__model--form--input-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" placeholder="Password" id="password" autoComplete="off" />
+                            </div>
+
+                            <div className="open-model-form__model--form--input-group">
+                                <div>
+                                    <button>Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                )}
 
                 <header className="open-model-form__header">Header</header>
 
@@ -35,7 +47,7 @@ export default function OpenModelForm() {
                         <li>Contact Us</li>
                         <li>About Us</li>
 
-                        <li>Login</li>
+                        <li onClick={loginOpenHandler}>Login</li>
                         <li>Register</li>
                     </ul>
                 </nav>
